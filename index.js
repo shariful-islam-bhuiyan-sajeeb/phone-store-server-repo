1468e9f4d6658eae5da8d816034a51f6a6c4e065
shariful4345 <mdsajeeb4345@gmail.com>
2022-11-24 17:24:56 +0600

    mobileCollction data commit

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
 //middle were
 app.use(cors())
 app.use(express())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y8s6gcn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const phoneStoreCollection = client.db('mobileStore').collection('mobileCollection')
+
+        app.get('/mobileCollection', async(req ,res) =>{
+            const query = {};
+            const options = await phoneStoreCollection.find(query).toArray();
+            res.send(options)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log());
+
 
 app.get('/', async (req,res) =>{
     res.send('mobile store server is running')

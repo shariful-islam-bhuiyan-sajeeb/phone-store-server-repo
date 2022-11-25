1088a0025b4eef74445fa0b9b319a56063abfac4
shariful4345 <mdsajeeb4345@gmail.com>
2022-11-25 20:55:15 +0600

    dashboard bookings

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ async function run(){
             res.send(resallerAllCategory)
         })
 
+        app.get('/bookings', async (req,res) =>{
+            const email = req.query.email;
+            const query = {email : email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings)
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body
             console.log(booking);
